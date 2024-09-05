c6980067ff6a50e9bfe94db5b791ef9406b82cf8
drtorres <drt2856@gmail.com>
2024-09-05 10:31:27 -0400

    Update of mi cv an carrer

--- src/components/Carrer.jsx
+++ src/components/Carrer.jsx
@@ -1,82 +1,92 @@
-import { LANGUAGES_SUPORTED } from '../context/lenguageContext';
-import { useLanguage } from '../hooks/UseLanguage';
-import './Timeline.css'
+import { LANGUAGES_SUPORTED } from "../context/lenguageContext";
+import { useLanguage } from "../hooks/UseLanguage";
+import "./Timeline.css";
 
 export function Carrer() {
-    const { language } = useLanguage();
+  const { language } = useLanguage();
 
-    return <section id="carrera">
-        <div className="container my-5" >
-            <div className="row">
-                <div className="col-md-12">
-                    <div className="card">
-                        <div className="card-body">
-                            <h2 className="card-title m-3">
-                                {language === LANGUAGES_SUPORTED.ESPAÑOL ?
-                                    "Trayectoria Profesional"
-                                    :
-                                    "Career"
-                                }</h2>
-                            <div id="content">
-                                <ul className="timeline " >
-                                    <li className="event work" data-date="14/2/2023 - ...">
-                                        <h3 className='mb-2'>
-                                            {language === LANGUAGES_SUPORTED.ESPAÑOL ?
-                                                "Especialista Informático en la Central Termoeléctrica  Máximo Gómez de Mariel"
-                                                :
-                                                "Computer Specialist at the Máximo Gómez de Mariel Thermoelectric Power Plant"
-                                            }</h3>
-                                        <p>
-                                            {language === LANGUAGES_SUPORTED.ESPAÑOL ?
-                                                " En mi trayectoria en esta empresa, desempeñé diversos roles. Mi función principal consistía en ser el administrador de redes informáticas, supervisando y manteniendo el correcto funcionamiento de los servidores y las redes. Además, me encargaba de desplegar software informático y proteger los datos sensibles de los trabajadores."
-                                                :
-                                                " In my career in this company, I played various roles. My primary role was to be the computer network administrator, overseeing and maintaining the proper functioning of servers and networks. In addition, I was in charge of deploying computer software and protecting the sensitive data of the workers."
-                                            }
-                                        </p>
-                                        <p>
-                                            {language === LANGUAGES_SUPORTED.ESPAÑOL ?
-                                                "Adicionalmente, asumí el rol de especialista encargado de implementar el nuevo programa de control de acceso a la empresa. Fui responsable de garantizar su correcta puesta en funcionamiento."
-                                                :
-                                                "Additionally, I assumed the role of specialist in charge of implementing the new access control program to the company. I was responsible for ensuring its proper commissioning."
-                                            }</p>
-                                        <p>
-                                            {language === LANGUAGES_SUPORTED.ESPAÑOL ?
-                                                "Por último, desarrollé la página web oficial de la empresa, que hasta ese momento no contaba con ninguna presencia en línea"
-                                                :
-                                                "Finally, I developed the company's official website, which until then did not have any online presence"
-                                            }</p>
-                                    </li>
-                                    <li className="event education" data-date="1/3/2019 - 20/12/22">
-                                        <h3>
-                                            {language === LANGUAGES_SUPORTED.ESPAÑOL ?
-                                                "Estudios de Ingeniería Informática en la Universidad Agraria de la Habana"
-                                                :
-                                                "Computer Engineering studies at the Universidad Agraria de la Habana"
-                                            }</h3>
-                                        <p>
-                                            {language === LANGUAGES_SUPORTED.ESPAÑOL ?
-                                                "Me gradué como ingeniero informático en la Universidad Agraria de La Habana, donde me preparé intensamente para el mundo laboral en diversas especialidades. Entre ellas, destacan el desarrollo de software, el desarrollo web, la ingeniería de requisitos y las investigaciones operacionales."
-                                                :
-                                                "I graduated as a computer engineer from the Universidad Agraria de La Habana, where I prepared intensely for the world of work in various specialties. These include software development, web development, requirements engineering, and operational research."
-                                            }</p>
-                                        <p>
-                                            {language === LANGUAGES_SUPORTED.ESPAÑOL ?
-                                                "Aunque mis estudios abarcaron varias áreas, mi enfoque principal se centró en el desarrollo web utilizando los frameworks Java Spring Boot y React.js. Me especialicé en estas tecnologías y continué mejorando mis habilidades. Además, participé en varios eventos de programación, obteniendo buenos resultados"
-                                                :
-                                                "Although my studies covered several areas, my main focus was on web development using the Java Spring Boot and React.js frameworks. I specialized in these technologies and continued to improve my skills. In addition, I participated in several programming events, obtaining good results"
-                                            }</p>
-                                        <p> {language === LANGUAGES_SUPORTED.ESPAÑOL ?
-                                            "En mi último año de carrera, tuve la oportunidad de practicar la docencia como profesor de la asignatura de introducción a la programación. Fue una experiencia enriquecedora que me permitió compartir mis conocimientos con otros estudiantes"
-                                            :
-                                            "In my last year of study, I had the opportunity to practice teaching as a professor of the introductory subject to programming. It was an enriching experience that allowed me to share my knowledge with other students"
-                                        }</p>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
+  return (
+    <section id="carrera">
+      <div className="container my-5">
+        <div className="row">
+          <div className="col-md-12">
+            <div className="card">
+              <div className="card-body">
+                <h2 className="card-title m-3">
+                  {language === LANGUAGES_SUPORTED.ESPAÑOL
+                    ? "Trayectoria Profesional"
+                    : "Career"}
+                </h2>
+                <div id="content">
+                  <ul className="timeline ">
+                    <li className="event work" data-date="14/2/2023 - ...">
+                      <h3 className="mb-2">
+                        {language === LANGUAGES_SUPORTED.ESPAÑOL
+                          ? "Especialista Informático en la Central Termoeléctrica  Máximo Gómez de Mariel"
+                          : "Computer Specialist at the Máximo Gómez de Mariel Thermoelectric Power Plant"}
+                      </h3>
+                      <p>
+                        {language === LANGUAGES_SUPORTED.ESPAÑOL
+                          ? " En mi trayectoria en esta empresa, desempeñé diversos roles. Mi función principal consistía en ser el administrador de redes informáticas, supervisando y manteniendo el correcto funcionamiento de los servidores y las redes. Además, me encargaba de desplegar software informático y proteger los datos sensibles de los trabajadores."
+                          : " In my career in this company, I played various roles. My primary role was to be the computer network administrator, overseeing and maintaining the proper functioning of servers and networks. In addition, I was in charge of deploying computer software and protecting the sensitive data of the workers."}
+                      </p>
+                      <p>
+                        {language === LANGUAGES_SUPORTED.ESPAÑOL
+                          ? "Adicionalmente, asumí el rol de especialista encargado de implementar el nuevo programa de control de acceso a la empresa. Fui responsable de garantizar su correcta puesta en funcionamiento."
+                          : "Additionally, I assumed the role of specialist in charge of implementing the new access control program to the company. I was responsible for ensuring its proper commissioning."}
+                      </p>
+                      <p>
+                        {language === LANGUAGES_SUPORTED.ESPAÑOL
+                          ? "Por último, desarrollé la página web oficial de la empresa, que hasta ese momento no contaba con ninguna presencia en línea"
+                          : "Finally, I developed the company's official website, which until then did not have any online presence"}
+                      </p>
+                    </li>
+                    <li className="event work" data-date="14/2/2023 - ...">
+                      <h3 className="mb-2">
+                        {language === LANGUAGES_SUPORTED.ESPAÑOL
+                          ? "Freelance BBDigital part-time"
+                          : "Freelance BBDigital part-time"}
+                      </h3>
+
+                      <p>
+                        {language === LANGUAGES_SUPORTED.ESPAÑOL
+                          ? "Contratado a tiempo parcial como Freelancer para una empresa privada con el rol de desarrollador web full stack. Trabajar de acuerdo con las solicitudes del proyecto"
+                          : "Hired part-time as a Freelancer for a private company with the role of full stack web developer. Working according to project requests"}
+                      </p>
+                    </li>
+                    <li
+                      className="event education"
+                      data-date="1/3/2019 - 20/12/22"
+                    >
+                      <h3>
+                        {language === LANGUAGES_SUPORTED.ESPAÑOL
+                          ? "Estudios de Ingeniería Informática en la Universidad Agraria de la Habana"
+                          : "Computer Engineering studies at the Universidad Agraria de la Habana"}
+                      </h3>
+                      <p>
+                        {language === LANGUAGES_SUPORTED.ESPAÑOL
+                          ? "Me gradué como ingeniero informático en la Universidad Agraria de La Habana, donde me preparé intensamente para el mundo laboral en diversas especialidades. Entre ellas, destacan el desarrollo de software, el desarrollo web, la ingeniería de requisitos y las investigaciones operacionales."
+                          : "I graduated as a computer engineer from the Universidad Agraria de La Habana, where I prepared intensely for the world of work in various specialties. These include software development, web development, requirements engineering, and operational research."}
+                      </p>
+                      <p>
+                        {language === LANGUAGES_SUPORTED.ESPAÑOL
+                          ? "Aunque mis estudios abarcaron varias áreas, mi enfoque principal se centró en el desarrollo web utilizando los frameworks Java Spring Boot y React.js. Me especialicé en estas tecnologías y continué mejorando mis habilidades. Además, participé en varios eventos de programación, obteniendo buenos resultados"
+                          : "Although my studies covered several areas, my main focus was on web development using the Java Spring Boot and React.js frameworks. I specialized in these technologies and continued to improve my skills. In addition, I participated in several programming events, obtaining good results"}
+                      </p>
+                      <p>
+                        {" "}
+                        {language === LANGUAGES_SUPORTED.ESPAÑOL
+                          ? "En mi último año de carrera, tuve la oportunidad de practicar la docencia como profesor de la asignatura de introducción a la programación. Fue una experiencia enriquecedora que me permitió compartir mis conocimientos con otros estudiantes"
+                          : "In my last year of study, I had the opportunity to practice teaching as a professor of the introductory subject to programming. It was an enriching experience that allowed me to share my knowledge with other students"}
+                      </p>
+                    </li>
+                  </ul>
                 </div>
+              </div>
             </div>
+          </div>
         </div>
+      </div>
     </section>
+  );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,97 +1,132 @@
-import cv from "../assets/cv.pdf"
+import cv from "../assets/cv.pdf";
 import { LANGUAGES_SUPORTED } from "../context/lenguageContext";
-import { useLanguage } from "../hooks/UseLanguage"
+import { useLanguage } from "../hooks/UseLanguage";
 export function Header() {
   const { language, setPreferencedLanguage } = useLanguage();
 
-  return <section >
-    <header id="header" >
-      <nav className='navbar navbar-expand-lg bg-body-tertiary '>
-        <a className="navbar-brand mx-3 h2 " href="#">Full stack web developer</a>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse navbar-text " id="navbarSupportedContent">
-          <ul className='navbar-nav ms-auto'>
-            <li className='nav-item'>
-              <a className='nav-link' href="#proyectos">
-                {language === LANGUAGES_SUPORTED.ESPAÑOL ?
-                  "Proyectos"
-                  :
-                  "Projects"
-                }
-              </a>
-            </li>
-            <li className='nav-item'>
-              <a className='nav-link' href="#tecnologias">
-                {language === LANGUAGES_SUPORTED.ESPAÑOL ?
-                  "Tecnologías"
-                  :
-                  "Technologies"
-                }</a>
-            </li>
-            <li className='nav-item'>
-              <a className='nav-link' href="#carrera">
-                {language === LANGUAGES_SUPORTED.ESPAÑOL ?
-                  "Carrera profesional"
-                  :
-                  "Career"
-                }</a>
-            </li>
-            <li className='nav-item'>
-              <a className='nav-link' href="#contactos">
-                {language === LANGUAGES_SUPORTED.ESPAÑOL ?
-                  "Contacto"
-                  :
-                  "Contact"
-                }</a>
-            </li>
-            <li className="nav-item dropdown">
-              <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                {language === LANGUAGES_SUPORTED.ESPAÑOL ?
-                  "Idioma"
-                  :
-                  "Language"
-                }
-              </a>
-              <ul className="dropdown-menu">
-                <li><a className="dropdown-item" onClick={()=>setPreferencedLanguage(LANGUAGES_SUPORTED.ESPAÑOL)}>Español</a></li>
-                <li><a className="dropdown-item" onClick={()=>setPreferencedLanguage(LANGUAGES_SUPORTED.ENGLISH)}>English</a></li>
-              </ul>
-            </li>
-          </ul>
-
-        </div>
-
-      </nav>
-    </header>
-    <div className='wall p-5 blue-shadow '>
-      <div>
-        <h1 className='display-1 '> <b>Daniel Rodríguez Torres</b> </h1>
-        <h3 className="text-primary">
-          {language === LANGUAGES_SUPORTED.ESPAÑOL ?
-            "Desarrollador web full stack a su servicio"
-            :
-            "Full stack web developer at your service"
-          }
+  return (
+    <section>
+      <header id="header">
+        <nav className="navbar navbar-expand-lg bg-body-tertiary ">
+          <a className="navbar-brand mx-3 h2 " href="#">
+            Full stack web developer
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div
+            className="collapse navbar-collapse navbar-text "
+            id="navbarSupportedContent"
+          >
+            <ul className="navbar-nav ms-auto">
+              <li className="nav-item">
+                <a className="nav-link" href="#proyectos">
+                  {language === LANGUAGES_SUPORTED.ESPAÑOL
+                    ? "Proyectos"
+                    : "Projects"}
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#tecnologias">
+                  {language === LANGUAGES_SUPORTED.ESPAÑOL
+                    ? "Tecnologías"
+                    : "Technologies"}
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#carrera">
+                  {language === LANGUAGES_SUPORTED.ESPAÑOL
+                    ? "Carrera profesional"
+                    : "Career"}
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#contactos">
+                  {language === LANGUAGES_SUPORTED.ESPAÑOL
+                    ? "Contacto"
+                    : "Contact"}
+                </a>
+              </li>
+              <li className="nav-item dropdown">
+                <a
+                  className="nav-link dropdown-toggle"
+                  href="#"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  {language === LANGUAGES_SUPORTED.ESPAÑOL
+                    ? "Idioma"
+                    : "Language"}
+                </a>
+                <ul className="dropdown-menu">
+                  <li>
+                    <a
+                      className="dropdown-item"
+                      onClick={() =>
+                        setPreferencedLanguage(LANGUAGES_SUPORTED.ESPAÑOL)
+                      }
+                    >
+                      Español
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      className="dropdown-item"
+                      onClick={() =>
+                        setPreferencedLanguage(LANGUAGES_SUPORTED.ENGLISH)
+                      }
+                    >
+                      English
+                    </a>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </header>
+      <div className="wall p-5 blue-shadow ">
+        <div>
+          <h1 className="display-1 ">
+            {" "}
+            <b>Daniel Rodríguez Torres</b>{" "}
+          </h1>
+          <h3 className="text-primary">
+            {language === LANGUAGES_SUPORTED.ESPAÑOL
+              ? "Desarrollador web full stack a su servicio"
+              : "Full stack web developer at your service"}
           </h3>
-        <a className='btn btn-danger ' href={cv} download="CV.pdf">
-        {language === LANGUAGES_SUPORTED.ESPAÑOL ?
-            "Descargar curriculum"
-            :
-            "Download curriculum"
-          } <i className='bi bi-file-earmark-arrow-down' />
-        </a>
-        <a className='btn btn-purple m-2 ' href="https://github.com/drt2856">
-          GitHub <i className='bi bi-github' />
-        </a>
-        <a className='btn btn-primary' href="https://www.linkedin.com/in/daniel-rodr%C3%ADquez-torres-571424260/">
-          Linkedin <i className='bi bi-linkedin' />
-        </a>
+          <a className="btn btn-danger " href={cv} download="CV.pdf">
+            {language === LANGUAGES_SUPORTED.ESPAÑOL
+              ? "Descargar curriculum"
+              : "Download curriculum"}{" "}
+            <i className="bi bi-file-earmark-arrow-down" />
+          </a>
+          <a
+            className="btn btn-purple m-2 "
+            href="https://github.com/drt2856"
+            target="_blank"
+          >
+            GitHub <i className="bi bi-github" />
+          </a>
+          <a
+            className="btn btn-primary"
+            href="https://linkedin.com/in/daniel-rodriguez-torres-571424260"
+            target="_blank"
+          >
+            Linkedin <i className="bi bi-linkedin" />
+          </a>
+        </div>
       </div>
-
-    </div>
-
-
-  </section>
+    </section>
+  );
 }
